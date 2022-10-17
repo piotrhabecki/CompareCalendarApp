83dea2e0b0215510d8d6ef0d5ad6ad1bc7ea890b
phabecki <Piotr.Habecki@ingenico.com>
2022-10-17 23:57:21 +0200

    add link to about me page

---
+++ src/components/AboutPage/AboutPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <div className="pt-3 border-0 text-xl font-semibold pb-2 w-full text-center">
+        {" "}
+        <a href="https://testquest.vercel.app/" className="text-blue-600 underline">Test Quest</a>
+      </div>
+    </div>
+  );
+};
+
+export default AboutPage;
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -2,13 +2,15 @@ import React from "react";
 import CreateEventButton from "../CreateEventButton/CreateEventButton";
 import TimeSpan from "../TimeSpan/TimeSpan";
 import SmallCalendar from "../SmallCalendar/SmallCalendar";
+import AboutPage from "../AboutPage/AboutPage";
 
 function Sidebar() {
   return (
-    <aside className="border p-5 w-64">
+    <aside className="border p-5 h-screen">
       <CreateEventButton />
       <SmallCalendar />
       <TimeSpan />
+      <AboutPage />
     </aside>
   );
 }
